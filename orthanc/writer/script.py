4e8756f368edf36845d1fa4b885423949a4a83fb
Diego <diegovictorrj@gmail.com>
2023-09-06 04:23:59 +0100

    Create metric to measure total time to receive N studies

--- orthanc/writer/script.py
+++ orthanc/writer/script.py
@@ -15,6 +15,7 @@ httpClient.verify = False
 httpClient.request = functools.partial(httpClient.request, timeout = 60)
 
 processingStudies = {} # Hold a timestamp for each incoming study
+totalTime = datetime.min
 
 def requestFilter(uri, **request):
     auth = str(base64.b64decode(request['headers']['authorization'].split()[1]), 'utf-8')
@@ -25,18 +26,20 @@ def requestFilter(uri, **request):
 def onNewStudy(resourceId):
     study = httpClient.get(f"{ORTHANC_ROOT_URL}/studies/{resourceId}").json()
     processingStudies[study.get('MainDicomTags').get('StudyInstanceUID')] = datetime.now(timezone.utc)
-    orthanc.LogWarning(f"NEW STUDY: {resourceId}")
+    orthanc.LogWarning(f"NEW STUDY: {study.get('PatientMainDicomTags').get('PatientName')}")
 
 def onStableStudy(resourceId):
+    global totalTime
     study = httpClient.get(f"{ORTHANC_ROOT_URL}/studies/{resourceId}").json()
     studyInstanceUID = study.get('MainDicomTags').get('StudyInstanceUID')
     studyCreationDate = processingStudies[studyInstanceUID]
 
     delta = datetime.now(timezone.utc) - studyCreationDate - timedelta(seconds = CONFIGURATION.get('StableAge'))
+    totalTime += delta
     processingStudies.pop(studyInstanceUID, None)
 
-    orthanc.LogWarning(f"STABLE. ELAPSED TIME: {delta}")
-    orthanc.LogWarning(f"STUDY DATA: {study}")
+    orthanc.LogWarning(f"STABLE - {study.get('PatientMainDicomTags').get('PatientName')} - ELAPSED TIME: {delta}")
+    orthanc.LogWarning(f"TOTAL TIME - {totalTime}")
 
 def onChange(changeType, level, resourceId):
     if changeType == orthanc.ChangeType.NEW_STUDY:
