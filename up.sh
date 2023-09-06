c7fcc95de12ce737f4436756c0b369b5658af16f
Diego <diegovictorrj@gmail.com>
2023-09-06 01:05:58 +0100

    Fix up.sh

--- up.sh
+++ up.sh
@@ -10,6 +10,10 @@ function writers() {
     docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --build -d --force-recreate --no-deps general-writer xray-writer
 }
 
+function nginx() {
+    docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --build -d --force-recreate --no-deps nginx
+}
+
 function all() {
     docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --build -d --force-recreate
 }
